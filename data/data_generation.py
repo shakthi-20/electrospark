e4cb35481bac5ff5bdcfa702fd8d69f3b01619df
shakthi-20 <84408357+shakthi-20@users.noreply.github.com>
2024-03-31 00:16:14 +0530

    Update data_generation.py

--- data/data_generation.py
+++ data/data_generation.py
@@ -5,14 +5,14 @@ import random
 
 # Generate random data
 data = {
-    "Soil Moisture": [random.randint(0, 100) for _ in range(100)],
-    "Temperature": [random.randint(10, 40) for _ in range(100)],
-    "Nutrient Levels": [random.randint(0, 5) for _ in range(100)],
-    "Acidity (pH)": [random.uniform(4, 8) for _ in range(100)],
-    "Pest Activity": [random.randint(0, 10) for _ in range(100)],
-    "Oxygen Levels": [random.randint(10, 25) for _ in range(100)],
-    "Manure Requirements": [random.randint(0, 5) for _ in range(100)],
-    "Weed Presence": [random.randint(0, 1) for _ in range(100)]
+    "Soil Moisture": [random.randint(0, 100) for _ in range(50)],
+    "Temperature": [random.randint(10, 40) for _ in range(50)],
+    "Nutrient Levels": [random.randint(0, 5) for _ in range(50)],
+    "Acidity (pH)": [random.uniform(4, 8) for _ in range(50)],
+    "Pest Activity": [random.randint(0, 10) for _ in range(50)],
+    "Oxygen Levels": [random.randint(10, 25) for _ in range(50)],
+    "Manure Requirements": [random.randint(0, 5) for _ in range(50)],
+    "Weed Presence": [random.randint(0, 1) for _ in range(50)]
 }
 
 # Create DataFrame
